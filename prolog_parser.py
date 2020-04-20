9a7041a662ab889a0afd331f05adbbc04035716b
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2020-04-20 14:45:48 +0100

    Removed a test case, and implemented an ad-hoc equivalence check.

--- prolog_parser.py
+++ prolog_parser.py
@@ -1,7 +1,7 @@
 __author__ = "cloudstrife9999"
 
 from re import match
-from typing import Optional
+from typing import Optional, Tuple
 
 if hasattr(__import__("re"), "Match"):
     from re import Match
@@ -42,11 +42,32 @@ class PrologOutputParser():
 
         outcome: bool = result == expected_result
         
+        title: str = "\"Prolog Primer\""
+
+        ##### BEGIN ASSIGNMENT_2 EQUIVALENCE CLASS #####
+        # TODO: implement the equivalence of multiple solutions in a more scalable way.
+        if not outcome and title in expected_result:
+            new_outcome, result = self.__check_for_prolog_primer_book_title_equivalence(result=result, expected_result=expected_result, title=title)
+            outcome |= new_outcome
+        
+        ##### END ASSIGNMENT_2 EQUIVALENCE CLASS #####
+
         # If order does not matter.
         if not outcome and out_of_order_allowed:
             outcome |= self.__ordered_match(result, expected_result)
 
         return outcome
+
+    # TODO: implement the equivalence of multiple solutions in a more scalable way.
+    def __check_for_prolog_primer_book_title_equivalence(self, result: str, expected_result: str, title: str) -> Tuple[bool, str]:    
+        if not title in expected_result:
+            return False, result
+
+        for candidate in ("'Prolog Primer'", "'prolog primer'", "\"prolog primer\""):
+            if candidate in result:
+                result = result.replace(candidate, title)
+
+        return result == expected_result, result
 
     def __ordered_match(self, unordered: str, ordered: str) -> bool:
         assert match(self.__result_regex, ordered)
