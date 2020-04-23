2e97f0b851bf5f2d80b960263efa59e770a5464a
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2020-04-23 17:04:28 +0100

    Commented out the test case which was meant to check whether a check that pass(X) --> student(X) was performed.

--- tests2/test.pl
+++ tests2/test.pl
@@ -3,4 +3,4 @@ pass_year(bob) :- pass(bob).
 pass_year(mary) :- pass(mary).
 pass_year(tony) :- pass(tony).
 pass_year(pat) :- pass(pat).
-pass_year(you_definitely_do_not_pass) :- pass(you_definitely_do_not_pass).
+%pass_year(you_definitely_do_not_pass) :- pass(you_definitely_do_not_pass).
